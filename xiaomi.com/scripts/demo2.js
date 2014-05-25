73547fbe661089dc13646b4bd0208bba599d21e0
zhanglun <zhanglun1410@gmail.com>
2014-05-25 17:17:14 +0800

    commit

--- xiaomi.com/scripts/demo2.js
+++ xiaomi.com/scripts/demo2.js
@@ -22,29 +22,33 @@
     if (typeof window.addEventListener === "function") {
         MYAPP.subl.addListener = function(elem, type, handler) {
             elem.addEventListener(type, handler, false);
-        }
+        };
     } else if (typeof document.attachEvent === "function") {
         MYAPP.subl.addListener = function(elem, type, handler) {
             elem.attachEvent("on" + type, handler);
-        }
+        };
     } else {
         MYAPP.subl.addListener = function(elem, type, handler) {
             elem["on" + type] = handler;
-        }
+        };
     }
 
     MYAPP.subl.autoLoadCommand = function(commands) {
+
         var length = commands.length,
             currentCommand = commands[0],
-            html = "<li class=\"current\">" + currentCommand + "</li>"
+            html = "<li class=\"current\">" + currentCommand + "</li>";
         for (var i = 1; i < length; i++) {
             html += ("<li>" + commands[i] + "</li>");
         }
         this.listContainer.html(html);
-    }
+
+    };
 
     MYAPP.subl.togglePanel = function(panel, event) {
+
         var $panel=$(panel);
+
         if (event.ctrlKey && event.shiftKey && event.keyCode == 80) {
             if ($panel.css("display") == "none") {
                 $panel.show();
@@ -61,12 +65,12 @@
 
 
     MYAPP.subl.selectCommand = function(commandList,key) {
-
         var $list = $(commandList),
             $commands=$("li",$list);
             length = $commands.size(),
-            currentLine = $(".current", $list),
-            count = $list.index($(currentLine));
+            currentLine = $(".current", $list).eq(0),
+            count = $commands.index(currentLine);
+
 
         if (key == 38 && count > 0) {
             currentLine.removeClass("current");
@@ -83,26 +87,44 @@
     //查找 字符所在的位置，高亮，返回高亮之后的字符串
 
     MYAPP.subl.highlightChar = function(targetStr, referStr) {
-        var refLen = referStr.length,
-            tarLen = targetStr.length;
-
+        var refLen = referStr.length;
         for (var j = 0; j < refLen; j++) {
+            /*
+            *这里有点绕
+            *
+            *每次先将文本中的 "<b></b>" 替换成 "-+"，防止匹配标签中的 b 字母
+            *
+            *然后为匹配的字母加上标签
+            *
+            *最后将之前替换后的 "-+" 还原成标签
+            *
+            *没有想到更好的解决办法
+            */
             var pattern = new RegExp(referStr[j],"g");
+
+            targetStr=targetStr.replace(/\<.\>/g,"-");
+            targetStr=targetStr.replace(/\<\/.\>/g,"+");
             targetStr = targetStr.replace(pattern, "<b>" + referStr[j] + "</b>");
+
+            targetStr=targetStr.replace(/\-/g, "<b>");
+            targetStr=targetStr.replace(/\+/g, "</b>");
+            
         }
 
         return targetStr;
     };
 
 
-    MYAPP.subl.filterCommand = function(commandsContainer,commands,value) {
+    MYAPP.subl.filterCommand = function(commandsContainer,value) {
 
         var list = $(COMMANDS),
             length = list.size(),
             i = 0;
+
         var commandStore = []; //存放匹配到的li
 
         //检查输入的字符，外层循环命令中的字符串，每个字符与value的每个字符匹配
+
         while (i < length) {
             var cur = list[i],
                 node={}; //保存相关信息
@@ -122,17 +144,16 @@
             //如果匹配到的次数小于查询字符串的长度，则mark为false，不存入数组;
             node.mark = (node.matchNum == value.length);
             if (node.mark) {
-                node.dom = "<li>"+this.highlightChar(cur, value)+"</li>";
+                node.value = this.highlightChar(cur, value);
                 commandStore.push(node);
             }
             i++;
         }
+
         //将匹配的字符 重新排序
         var reOrder = function(commandArr) {
             var length = commandArr.length;
-            var frag = document.createDocumentFragment();
 
-            //冒泡排序
             for (var i = 0; i < length - 1; i++) {
                 for (var j = 0; j < length - 1 - i; j++) {
 
@@ -147,17 +168,17 @@
                 }
 
             }
-
-            var html="";
-            for (var i = 0; i < length; i++) {
-                html+=commandArr[i].dom;
+            var html="<li class=\"current\">"+commandArr[0].value+"</li>";
+            for (var g = 1; g < length; g++) {
+                html+="<li>"+commandArr[g].value+"</li>";
             }
             commandsContainer.innerHTML=html;
+        };
+        if(commandStore.length>0){
+            reOrder(commandStore);
         }
 
-        reOrder(commandStore);
-
-    }
+    };
 
 
 
@@ -167,24 +188,21 @@
 
         var input = document.getElementById("input"),
             commandList=document.getElementById("orderList"),
-            commands=commandList.getElementsByTagName("li"),
-            panel=document.getElementById("panel");;
+            panel=document.getElementById("panel");
+
         //ctrl+shift+P 显示面板。esc退出
+
         MYAPP.subl.addListener(document, "keydown", function(event) {
             MYAPP.subl.togglePanel(panel, event);
             MYAPP.subl.selectCommand(commandList, event.keyCode);
-            // console.log(event.keyCode);
-            // if(event.keyCode == "8"){
-            //     var value=input.value;
-            //     MYAPP.subl.filterCommand(commandList,commands,value);
-            // }
+
         });
-        //
+        
         MYAPP.subl.addListener(input, "input", function() {
             var value = this.value;
-            MYAPP.subl.filterCommand(commandList, commands,value);
+            MYAPP.subl.filterCommand(commandList,value);
         });
-    }
+    };
 
     MYAPP.subl.addListener(window, "load", appInit);
 
