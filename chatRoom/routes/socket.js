8cf4f7bbe411b9156ea5264a0e155ac939662fb7
zhanglun <zhanglun1410@gmail.com>
2014-06-04 23:15:29 +0800

    commit

--- chatRoom/routes/socket.js
+++ chatRoom/routes/socket.js
@@ -1,51 +1,12 @@
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
-
-var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.static(path.join(__dirname, 'public')));
-
-routes(app);
-
-// development only
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
-}
-
-
-//返回 http.Server() 实例
-var server = http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
-});
-
-var socket = require('./lib/chatServer');
-socket.listen(server);
-
-
-
-//var socket = require("socket.io"); //
-//var io = socket.listen(server); // 监听之前创建的 http.Server() 实例。返回一个io的实例
-
-
-(function() {
-    var userCount = 0,
+var socketio = require('socket.io'),
+    io;
+var userCount = 0,
         usersName = [];
+
+export.listen=function(){
+
+  io = socketio.listen(server);
+
     io.sockets.on('connection', function(socket) {
         userCount++;
 
@@ -96,4 +57,5 @@ socket.listen(server);
 
         });
     });
-})();
+
+}
