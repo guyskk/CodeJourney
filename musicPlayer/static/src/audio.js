b6c3bc9265c13b6cac1cc4b8f2d419df4469ff56
zhanglun <zhanglun1410@gmail.com>
2014-06-20 21:47:16 +0800

    commit

--- musicPlayer/static/src/audio.js
+++ musicPlayer/static/src/audio.js
@@ -2,45 +2,34 @@
 
 	function AudioControl(target){
 		this.target=target;
+		this.status=false;
 	}
 	AudioControl.prototype.addItem=function(name,elem){
 		this[name]=elem;
 	};
-	AudioControl.prototype.play=function(){
-		
-	};
 
-	window.onload=function(){
-		var audio = document.getElementsByTagName("audio")[0];
-		
-		var play=document.getElementById("control-play"),
-			backward=document.getElementById("control-backward"),
-			forward=document.getElementById("control-forward"),
-			volumeDown=document.getElementById("control-volume-down"),
-			volumeUp=document.getElementById("control-volume-up");
-
-
-		console.log(AudioControl);
-		var audioControl = new AudioControl(audio);
-
-		audioControl.addItem("play",play);
-		audioControl.addItem("backward",backward);
-		audioControl.addItem("forward",forward);
-		audioControl.addItem("volumeDown",volumeDown);
-		audioControl.addItem("volumeUp",volumeUp);
-		
-		play.status=false;
+	AudioControl.prototype.emitplay=function(){
+		var _this=this,
+			play=this.play,
+			audio=this.audio;
 		play.addEventListener("click",function(event){
-			if(play.status){
-				play.status=false;
+			if(_this.status){
+				_this.status=false;
 				play.title="播放";
 				audio.pause();
 			}else{
-				play.status=true;
+				_this.status=true;
 				play.title="暂停";
 				audio.play();
 			}
 		},false);
+	};
+
+	AudioControl.prototype.adjustVolume=function(){
+		var _this=this,
+			volumeDown=this.volumeDown,
+			volumeUp=this.volumeUp,
+			audio=this.audio;
 		volumeDown.addEventListener("click",function(event){
 			if(audio.volume>0){
 				audio.volume=(audio.volume-0.1).toFixed(1);
@@ -51,5 +40,34 @@
 				audio.volume=(audio.volume+0.1).toFixed(1);
 			}
 		},false);
+	};
+
+	window.onload=function(){
+
+		var audio = document.getElementsByTagName("audio")[0];
+		var play=document.getElementById("control-play"),
+			backward=document.getElementById("control-backward"),
+			forward=document.getElementById("control-forward"),
+			volumeDown=document.getElementById("control-volume-down"),
+			volumeUp=document.getElementById("control-volume-up");
+
+		var audioControl = new AudioControl(audio);
+
+		audioControl.addItem("audio",audio);
+		audioControl.addItem("play",play);
+		audioControl.addItem("backward",backward);
+		audioControl.addItem("forward",forward);
+		audioControl.addItem("volumeDown",volumeDown);
+		audioControl.addItem("volumeUp",volumeUp);
+		
+		audioControl.emitplay();
+		audioControl.adjustVolume();
+
+
+		audio.seekable.start();  // Returns the starting time (in seconds)
+		audio.seekable.end();    // Returns the ending time (in seconds)
+		audio.currentTime = 122; // Seek to 122 seconds
+		audio.played.end(); 
+
 	};
 })();
