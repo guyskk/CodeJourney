035b2297cdd38041fc43466dc43c709c965c5005
zhanglun <zhanglun1410@gmail.com>
2014-07-02 23:00:40 +0800

    commit by gvim

--- chatRoom/app.js
+++ chatRoom/app.js
@@ -31,7 +31,7 @@ if ('development' == app.get('env')) {
 
 //返回 http.Server() 实例
 var server = http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port http://127.0.0.1:' + app.get('port'));
 });
 
 var socket = require('./lib/chatServer');
