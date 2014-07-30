79f8e25afcf4a6850b89b7a10566a538b30cbc42
zhanglun <zhanglun1410@gmail.com>
2014-07-30 23:31:37 +0800

    add package.json and start node http  server

--- nodejs/server/server.js
+++ nodejs/server/server.js
@@ -1,7 +1,14 @@
 var http=require("http");
+var fs=require("fs");
+var path=require("path");
+var mime=require("mime");
+var cache={};
 
 http.createServer(function(req,res){
-	res.write({
-	    Contentdd
-	    });
-	});<F3>
+    res.writeHead(200,{
+      "Content-Type":"text/plain"
+    });
+res.write("Hello!");
+}).listen(3000,function(){
+  console.log("Server is started! port:3000");
+});
