039defbe972fb6c7a037163d911e729934520023
zhanglun <zhanglun1410@gmail.com>
2014-07-31 19:17:15 +0800

    add gruntfile.js

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -78,7 +78,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-less');
-
+    grunt.loadNpmTasks('grunt-');
 
     grunt.registerTask('test', ['jshint', 'watch']);
     grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'less']);
