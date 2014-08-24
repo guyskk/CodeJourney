90d64e9016c1f6b4031462b0b821edc09619b763
zhanglun <zhanglun1410@gmail.com>
2014-08-24 13:51:25 +0800

    commit

---
+++ netrobot/nodejs/app.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+var request = require('request');
+
+var luowang = 'http://www.luoo.net/music/';
+
+var luoo_dir = './luoo/';
+
+function saveContent(content){
+
+    var dir = luoo_dir + 'maganize.html';
+
+    fs.exists(dir, function(exists){
+        if(exists){
+            fs.writeFile(dir, content, function(err){
+                if(err){
+                    console.log(err);
+                }
+                console.log(content);
+            });  
+        }else{
+            fs.openSync(dir, 'w', function(err, content){
+              if(err){
+                  console.log(err);
+            }
+            console.log(typeof content);
+        
+            });
+        }
+    });    
+}
+
+
+request(luowang, function(error, response, body){
+    saveContent(body);
+});
+
+
