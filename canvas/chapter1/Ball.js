3a4a323ed4c967bd84e1e88b7e0d0d5b400bb586
zhanglun <zhanglun1410@gmail.com>
2014-09-29 15:02:06 +0800

    add Velocity.html

--- canvas/chapter1/Ball.js
+++ canvas/chapter1/Ball.js
@@ -1,8 +1,8 @@
-function Ball(radius, color){
-    if(radius === undefined){
+function Ball(radius, color) {
+    if (radius === undefined) {
         radius = 40;
     }
-    if(color === undefined){
+    if (color === undefined) {
         color = "#f00";
     }
     this.x = 0;
@@ -14,20 +14,54 @@ function Ball(radius, color){
     this.scaleY = 1;
     this.lineWidth = 1;
 }
-
-Ball.prototype.draw = function(context){
+Ball.prototype.draw = function(context) {
     context.save();
     context.translate(this.x, this.y);
     context.rotate(this.rotation);
     context.scale(this.scaleX, this.scaleY);
+    context.beginPath();
     context.lineWidth = this.lineWidth;
     context.fillStyle = this.color;
-    context.arc(0, 0, this.radius, 0, (Math.PI * 2), true);
+    context.arc(this.x, this.y, this.radius, 0, (Math.PI * 2), true);
     context.closePath();
     context.fill();
-    if(this.lineWidth > 0){
+    if (this.lineWidth > 0) {
         context.stroke();
     }
     context.restore();
 
 };
+
+function Cricle(x, y) {
+    if (x == undefined) {
+        x = 0;
+    }
+    if (y == undefined) {
+        y = 0;
+    }
+    this.x = x;
+    this.y = y;
+    this.radius = 10;
+    this.width = 20;
+    this.height = 20;
+}
+Cricle.prototype.draw = function(context) {
+    // context.save();
+    this.x += 2;
+    this.y += 2;
+    this.r = Math.round(Math.random() * 255);
+    this.g = Math.round(Math.random() * 255);
+    this.b = Math.round(Math.random() * 255);
+    this.rgba = "rgba(" + this.r + "," + this.g + "," + this.b + ",1)";
+    
+    //画园
+    context.fillStyle = this.rgba;
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, (Math.PI * 2), true);
+    context.closePath();
+    context.fill();
+    // 画方
+    /*        context.strokeStyle = this.rgba;
+        context.strokeRect(this.x, this.y, this.width, this.height);*/
+
+}
