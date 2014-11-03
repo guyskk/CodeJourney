23cd1b6e170dc1958faf2736f9837380e34d7c59
zhanglun <zhanglun1410@gmail.com>
2014-11-03 22:43:48 +0800

    Route

--- nodejs/demo/server.js
+++ nodejs/demo/server.js
@@ -1,6 +1,6 @@
 var http =require("http"),
     path = require("path"),
-    query = require("querystring"),
+    querystring = require("querystring"),
     fs = require("fs");
 
 var extensions = {
@@ -12,21 +12,36 @@ var extensions = {
     ".jpg": "image/jpeg"
 }
 
+function isEmpty(obj){
+    for (var name in obj) 
+    {
+        return false;
+    }
+    return true;
+}
 
 http.createServer(function(req, res){
 
-    var qs = querystring.parse(req.url.split("?")[1]),
-        // property names are the same as in the querystring
-        userName = qs.firstName + " " + qs.lastName;
+    var qs = querystring.parse(req.url.split("?")[1]);
 
-    console.log(useName);
+        // property names are the same as in the querystring
+        // userName = qs.firstName + " " + qs.lastName;
+
 
     var filename = path.basename(req.url) || "index.html",
         ext = path.extname(filename),
         dir = path.dirname(req.url).substring(1),
         localPath = __dirname + "/public/";
-        
+
+    if(!isEmpty(qs)){
+        userName = qs.username;
+        html = "<!doctype html>" +
+                "<html><head><title>Hello " + userName + "</title></head>" +
+                "<body><h1>Hello, " + userName + "!</h1></body></html>";
+        res.end(html);
+    }
     if(extensions[ext]){
+        console.log("file");
         localPath += (dir ? dir + "/" : "") + filename;
         fs.exists(localPath, function(exists){
             if(exists){
@@ -37,6 +52,7 @@ http.createServer(function(req, res){
             }
         });
     }
+
 
 }).listen(1234, "127.0.0.1");
 
