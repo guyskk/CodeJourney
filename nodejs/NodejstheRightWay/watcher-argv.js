43c735b1278e0207ae7de75eb3e956104d9c717e
zhanglun <zhanglun1410@gmail.com>
2014-11-07 15:28:02 +0800

    command line width arguments

---
+++ nodejs/NodejstheRightWay/watcher-argv.js
@@ -0,0 +1,12 @@
+var fs = require("fs"),
+    filename = process.argv[2];
+
+if(!filename){
+    console.log('A file to watch must be specified!');
+}
+
+fs.watch(filename, function(){
+    console.log("File " + filename + " just changed!");
+});
+
+console.log("Now watching " + filename + " for changes...");
