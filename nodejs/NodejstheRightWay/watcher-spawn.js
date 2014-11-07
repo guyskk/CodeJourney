1272dc848c548e56db740bf72fde99ea1afbaaf8
zhanglun <zhanglun1410@gmail.com>
2014-11-07 17:16:48 +0800

    hotfix

--- nodejs/NodejstheRightWay/watcher-spawn.js
+++ nodejs/NodejstheRightWay/watcher-spawn.js
@@ -5,7 +5,7 @@ if (!filename) {
     throw Error("A file to watch must be specified!");
 }
 fs.watch(filename, function() {
-    var ls = spawn('ls');
+    var ls = spawn('ls', ['-lh', filename]);
     ls.stdout.pipe(process.stdout);
 });
 console.log("Now watching " + filename + " for changes...");
