ae8f5dd588824d436a012f4333a2faf8fc84c42c
zhanglun <zhanglun1410@gmail.com>
2015-01-18 21:48:02 +0800

    commit

---
+++ nodejs/demo/app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by CrispElite on 2015/1/18 0018.
+ */
+var http = require('http');
+var fs = require('fs');
+
+var counter = require('./util/counter.js');
+
+console.log(counter.count());
+console.log(counter.count());
+console.log(counter.count());
+
+
+
+
---
+++ nodejs/demo/util/counter.js
@@ -0,0 +1,12 @@
+/**
+ * Created by CrispElite on 2015/1/18 0018.
+ */
+
+var i = 0;
+
+function count(){
+    return i++;
+}
+
+
+exports.count = count;
