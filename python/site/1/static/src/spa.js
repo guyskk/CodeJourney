1ad2ce48b36f7e13372b9f9deee2802c1ac9fcde
zhanglun <zhanglun1410@gmail.com>
2015-02-14 09:48:50 +0800

    backbone learning base finished

--- python/site/1/static/src/spa.js
+++ python/site/1/static/src/spa.js
@@ -1,11 +1,11 @@
 /**
  * Created by CrispElite on 2015/1/25 0025.
  */
-(function () {
-    var go = function () {
+(function() {
+    var go = function() {
         console.log('go');
     };
-    var go2 = function () {
+    var go2 = function() {
         console.log("go2");
     };
     var obj = {};
@@ -17,9 +17,9 @@
     obj.trigger('alert', 'Mother Fucker!');
 })();
 
-(function () {
+(function() {
     var Sidebar = Backbone.Model.extend({
-        promptColor: function () {
+        promptColor: function() {
             var cssColor = prompt('请输入一个CSS颜色值：');
             this.set({
                 color: cssColor
@@ -27,7 +27,7 @@
         }
     });
     window.sidebar = new Sidebar();
-    sidebar.bind('change:color', function (model, color) {
+    sidebar.bind('change:color', function(model, color) {
         $('#sidebar').css({
             background: color
         });
@@ -36,19 +36,19 @@
         color: 'white'
     });
 
-    $("#theme-btn").on('click', function () {
+    $("#theme-btn").on('click', function() {
         sidebar.promptColor();
     });
 
     var Man = Backbone.Model.extend({
         url: '/user',
-        initialize: function () {
+        initialize: function() {
             console.log('Hey, you create me!');
-            this.bind('change:name', function () {
+            this.bind('change:name', function() {
                 var name = this.get('name');
                 console.log('you change the name as:' + name);
             });
-            this.bind('error', function (model, error) {
+            this.bind('error', function(model, error) {
                 console.log(error);
             });
         },
@@ -56,31 +56,36 @@
             name: '张伦',
             age: '21'
         },
-        validate: function (attributes) {
+        validate: function(attributes) {
             if (attributes.name == '') {
                 return "name 不能为空！"
             }
         },
-        aboutMe: function () {
+        aboutMe: function() {
             return 'My name is' + this.get('name') + ', I am ' + this.get('age') + " years old.";
         }
     });
 
     var man = new Man();
-    man.set({name: 'CrispElite', age: 21});
+    man.set({
+        name: 'CrispElite',
+        age: 21
+    });
     man.save();
 
 
     var man1 = new Man;
-    man1.fetch({url: '/user/'});
+    man1.fetch({
+        url: '/user/'
+    });
     man1.fetch();
     man1.fetch({
         url: '/user/',
-        success: function (model, response) {
+        success: function(model, response) {
             console.log('success');
             console.log(model.get('0')['name']);
         },
-        error: function () {
+        error: function() {
             console.error(error);
         }
     });
@@ -88,21 +93,21 @@
 
 })();
 
-$(document).ready(function () {
+$(document).ready(function() {
 
     var SearchView = Backbone.View.extend({
-        initialize: function () {
-            console.log('Alerts suck!');
+        initialize: function() {
+            console.log('init a SearchView!');
             this.render();
         },
-        render: function () {
+        render: function() {
             var template = _.template($("#search_template").html(), {});
             this.$el.html(template);
         },
         events: {
             "click input[type=button]": 'doSearch'
         },
-        doSearch: function () {
+        doSearch: function() {
             alert("Search for " + $("#search_input").val());
         }
     });
@@ -110,31 +115,9 @@ $(document).ready(function () {
     var search_view = new SearchView({
         el: $("#search_container")
     });
-
-    var AppRouter = Backbone.Router.extend({
-        routes: {
-//            "*actions": "defaultRoute",
-            "posts/:id": "getPost",
-            "download/*path": "downloadFiles",
-            ":route/:action": "loadView"
-        }
+    search_view.render({
+        search_label: "搜索渲染"
     });
-
-    var app_router = new AppRouter();
-    app_router.on("route:defaultRoute", function (actions) {
-        console.log(actions);
-    });
-    app_router.on("route:getPost", function (id) {
-        console.log(id);
-    });
-    app_router.on("route:downloadFiles", function (path) {
-        console.log(path);
-    });
-    app_router.on("route:loadView", function (route, action) {
-        console.log(route + "___" + action);
-    });
-
-    Backbone.history.start();
 
 
     var Song = Backbone.Model.extend({
@@ -143,7 +126,7 @@ $(document).ready(function () {
             name: 'Not specified',
             artist: 'Not specified'
         },
-        initialize: function () {
+        initialize: function() {
             console.log("Music is the answer!");
         }
     });
@@ -153,25 +136,69 @@ $(document).ready(function () {
         url: '/songs'
     });
 
-    var song1 = new Song({name: "How Bizarre", artist: "OMC"});
-    var song2 = new Song({name: "Sexual Healing", artist: "Marvin Gaye"});
+    var song1 = new Song({
+        name: "How Bizarre",
+        artist: "OMC"
+    });
+    var song2 = new Song({
+        name: "Sexual Healing",
+        artist: "Marvin Gaye"
+    });
     var myAlbum = new Album([song1, song2]);
     console.log(myAlbum.models[0].get("name"));
 
     myAlbum.fetch({
-      success: function(collection, response, options){
-        collection.each(function(item){
-          console.log(item.get('name'));
-        });
-      },
-      error: function(collection, response, options){
-        console.log('error');
-        console.dir(response);
-      }
+        success: function(collection, response, options) {
+            collection.each(function(item) {
+                console.log(item.get('name'));
+            });
+        },
+        error: function(collection, response, options) {
+            console.log('error');
+            console.dir(response);
+        }
     });
 
 
     myAlbum.create([song1, song2]);
 
+
+    var AppRouter = Backbone.Router.extend({
+        routes: {
+            //            "*actions": "defaultRoute",
+            "posts/:id": "getPost",
+            "download/*path": "downloadFiles",
+            ":route/:action": "loadView",
+            "manual": "manual"
+        }
+    });
+
+    var app_router = new AppRouter();
+
+    app_router.on("route:defaultRoute", function(actions) {
+        console.log(actions);
+    });
+
+    app_router.on("route:getPost", function(id) {
+        console.log(id);
+    });
+
+    app_router.on("route:downloadFiles", function(path) {
+        console.log(path);
+    });
+
+    app_router.on("route:loadView", function(route, action) {
+        console.log(route + "___" + action);
+    });
+
+    app_router.on("route:manual", function() {
+        alert("call manual");
+        app_router.navigate("/posts/" + 404, {
+            tirgger: true,
+            replace: true
+        });
+    })
+
+    Backbone.history.start();
 
 });
