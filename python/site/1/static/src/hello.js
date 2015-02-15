8574875d0d58afcb98a82a452c1ea17dbc9cc887
zhanglun <zhanglun1410@gmail.com>
2015-02-15 14:41:46 +0800

    backbone turtorial 5.js

--- python/site/1/static/src/hello.js
+++ python/site/1/static/src/hello.js
@@ -1,63 +1,111 @@
-(function($){
+(function($) {
 
-  // Model Item
-  var Item = Backbone.Model.extend({
-    defaults:{
-      part1: 'hello',
-      part2: 'world'
-    }
-  });
+    // Model Item
+    var Item = Backbone.Model.extend({
+        defaults: {
+            part1: 'hello',
+            part2: 'world'
+        }
+    });
 
-  // Collection List
-  var List = Backbone.Collection.extend({
-    model: Item
-  });
+    // Collection List
+    var List = Backbone.Collection.extend({
+        model: Item
+    });
 
-  var ListView = Backbone.View.extend({
+    var ItemView = Backbone.View.extend({
+        tagName: 'li',
 
-    el: $('body'),
-    events: {
-      'click button#add': 'addItem'
-    },
+        initialize: function() {
+            _.bindAll(this, 'render', 'unrender');
+            this.model.bind('change', this.render);
+            this.model.bind('remove', this.unrender);
+            this.model.bind('remove', this.test);
+        },
 
-    initialize: function(){
-      _.bindAll(this, 'render', 'addItem', 'appendItem');
+        events: {
+            'click span.swap': 'swap',
+            'click span.delete': 'remove'
+        },
 
-      this.collection = new List();
-      this.collection.bind('add', this.appendItem);
+        render: function() {
+            $(this.el).html('<span>' + this.model.get('part1') + ' ' + this.model.get('part2') + '</span> &nbsp; &nbsp; <span class="swap" style="font-family:sans-serif; color:blue; cursor:pointer;">[swap]</span> <span class="delete" style="cursor:pointer; color:red; font-family:sans-serif;">[delete]</span>');
+            return this; // for chainable calls, like .render().el
+        },
 
-      this.counter = 0;
-      this.render();
-    },
+        unrender: function() {
+            console.log(this);
+            $(this.el).remove();
+        },
 
-    render: function(){
+        test: function(){
+            console.log(this);
+        },
 
-      var self = this;
+        swap: function() {
+            var swapped = {
+                part1: this.model.get('part2'),
+                part2: this.model.get('part1')
+            };
+            this.model.set(swapped);
+        },
 
-      $(this.el).append('<button id="add">Add list item</button>');
-      $(this.el).append('<ul></ul>');
+        remove: function() {
+            this.model.destroy();
 
-      // underscore _().each();
-      _(this.collection.models).each(function(item){
-        self.appendItem(item);
-      });
-    },
+        }
 
-    addItem: function(){
-      this.counter++;
-      var item = new Item();
-      item.set({
-        part2: item.get('part2') + this.counter + '(update time: ' + new Date() + ')'
-      });
-      this.collection.add(item);
-    },
+    });
 
-    appendItem: function(item){
-      $('ul', this.el).append('<li>'+ item.get('part1') + ' ' + item.get('part2') + '</li>');
+    var ListView = Backbone.View.extend({
 
-    }
-  });
+        el: $('body'),
 
-  var listView = new ListView();
+        // 绑定事件
+        events: {
+            'click button#add': 'addItem'
+        },
+
+        // 初始化
+        initialize: function() {
+            _.bindAll(this, 'render', 'addItem', 'appendItem');
+
+            this.collection = new List();
+            this.collection.bind('add', this.appendItem);
+
+            this.counter = 0;
+            this.render();
+        },
+
+        render: function() {
+            var self = this;
+
+            $(this.el).append('<button id="add">Add list item</button>');
+            $(this.el).append('<ul></ul>');
+
+            // underscore _().each();
+            _(this.collection.models).each(function(item) {
+                self.appendItem(item);
+            }, this);
+        },
+
+        addItem: function() {
+            this.counter++;
+            var item = new Item();
+            item.set({
+                part2: item.get('part2') + this.counter + '(update time: ' + new Date() + ')'
+            });
+            this.collection.add(item);
+        },
+
+        appendItem: function(item) {
+            var itemView = new ItemView({
+                model: item
+            });
+            $('ul', this.el).append(itemView.render().el);
+        }
+    });
+
+    var listView = new ListView();
 
 })(jQuery);
