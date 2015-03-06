b4a0370e79f3ffd42cc8a9bd7dd9cbf2cfd7f28d
zhanglun <zhanglun1410@gmail.com>
2015-03-06 09:04:07 +0800

    commit

--- nodejs/chatRoom/js/common.js
+++ nodejs/chatRoom/js/common.js
@@ -1,1 +1,10 @@
 var app = angular.module('myApp', []);
+// app.run(function($rootscope){
+// 	$rootscope.name = 'zhanglun';
+// });
+
+app.controller('myController', ['$scope', function($scope){
+	$scope.person = {
+		name: 'zhanglun'
+	};
+}]);
